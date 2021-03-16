540065a4b24ba9192aecf7c1f6083d904349281e
nguacon01 <dungdm6191@gmail.com>
2021-03-16 13:25:57 +0100

    update requirements, .dockerignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 exec gunicorn --config gunicorn_config.py wsgi:app
+
+# docker run --name jwt_server --network work_repo-network -v /home/nguacon01/work/jwt_server:/jwt_server --restart always nguacon01/jwt_server:latest
